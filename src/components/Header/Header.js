8df523db95a79420ec836d52ab92a52698fabe45
Mohammad Ali <itsproali@gmail.com>
2022-04-04 03:35:56 +0600

    Navbar Added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { MenuIcon, XIcon } from '@heroicons/react/solid'
+import { Link } from 'react-router-dom';
 
 const Header = () => {
+    const [open, setOpen] = useState(false)
     return (
-        <div>
-            
-        </div>
+        <nav className='bg-pink-50 p-3 sticky top-0 shadow-md z-10'>
+            <div onClick={() => setOpen(!open)} className='text-white w-8 cursor-pointer md:hidden'>
+                    {open ? <XIcon></XIcon> : <MenuIcon></MenuIcon>}
+                </div>
+            <ul className='flex flex-col sm:flex-row items-center justify-center'>
+                <li className='p-2 mx-3 hover:text-pink-600 ease-linear duration-200 border-y-2 border-transparent hover:border-y-2 hover:border-pink-600'><Link to='/'>HOME</Link></li>
+                <li className='p-2 mx-3 hover:text-pink-600 ease-linear duration-200 border-y-2 border-transparent hover:border-y-2 hover:border-pink-600'><Link to='/reviews'>REVIEWS</Link></li>
+                <li className='p-2 mx-3 hover:text-pink-600 ease-linear duration-200 border-y-2 border-transparent hover:border-y-2 hover:border-pink-600'><Link to='/dashboard'>DASHBOARD</Link></li>
+                <li className='p-2 mx-3 hover:text-pink-600 ease-linear duration-200 border-y-2 border-transparent hover:border-y-2 hover:border-pink-600'><Link to='/blog'>BLOG</Link></li>
+                <li className='p-2 mx-3 hover:text-pink-600 ease-linear duration-200 border-y-2 border-transparent hover:border-y-2 hover:border-pink-600'><Link to='/about'>ABOUT</Link></li>
+            </ul> 
+        </nav>
     );
 };
 
