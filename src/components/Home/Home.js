06321f24aa4d373e3cb4f4d2dedff40a8214ff93
Mohammad Ali <itsproali@gmail.com>
2022-04-04 02:34:31 +0600

    Home Section Added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,16 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
+    const [users, setUsers] = useState([])
+    useEffect(() => {
+        fetch('review.json')
+            .then(res => res.json())
+            .then(data => setUsers(data))
+    }, [])
+    const total = users.slice(0, 3);
     return (
-        <div className='grid grid-cols-1 md:grid-cols-2 items-center mx-4 md:mx-16'>
-            <div className="main-image w-full text-center">
-                <img className='w-full' src="main-image.jpg" alt="Luxury Watch" />
+        <div className='mx-4 md:mx-16'>
+            <div className='grid grid-cols-1 md:grid-cols-2 items-center'>
+                <div className="main-image w-full text-center">
+                    <img className='w-full' src="main-image.jpg" alt="Luxury Watch" />
+                </div>
+
+                <div className="home-text">
+                    <h1 className="text-5xl font-bold text-pink-600">Premium Digital Watch</h1>
+                    <p className='text-1xl my-4 font-medium text-gray-500'>Beautiful, bright AMOLED display and the broadest range of all-day health monitoring features keeps track of your energy levels, respiration, menstrual cycle, stress, sleep, estimated heart rate and more</p>
+                    <button className="font-medium px-4 py-2 bg-pink-200 text-pink-500 cursor-pointer rounded hover:bg-pink-300 hover:text-pink-700 ease-in-out duration-300">Live Demo</button>
+                </div>
             </div>
 
-            <div className="home-text">
-                <h1 className="text-5xl font-bold text-pink-600">Premium Digital Watch</h1>
-                <p className='text-1xl my-4 font-medium text-gray-500'>Beautiful, bright AMOLED display and the broadest range of all-day health monitoring features keeps track of your energy levels, respiration, menstrual cycle, stress, sleep, estimated heart rate and more</p>
-                <button className="font-medium px-4 py-2 bg-pink-200 text-pink-500 cursor-pointer rounded hover:bg-pink-300 hover:text-pink-700 ease-in-out duration-300">Live Demo</button>
+            <div className="customer-reviews my-10">
+                <h2 className='text-4xl font-bold text-center my-5'>Customer Reviews ({users.length})</h2>
+
+                <div className="review-container grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3">
+                {
+                    total.map(user =>
+                        <div key={user.id} className="review p-3 rounded  shadow-lg hover:shadow-xl">
+                            <img className='w-[70px] h-[70px] block mx-auto rounded-full' src={user.img} alt={user.name} />
+                            <h3 className='font-bold text-2xl text-center my-1'>{user.name}</h3>
+                            <p>{ user.description}</p>
+                            <p className='font-bold'>Rating : { user.rating}</p>
+                        </div>
+                    )
+                }
+                </div>
+                
+                <button className="block mx-auto my-5 font-medium px-8 py-2 bg-pink-200 text-pink-500 cursor-pointer rounded hover:bg-pink-300 hover:text-pink-700 ease-in-out duration-300">See All Reviews</button>
             </div>
         </div>
     );
