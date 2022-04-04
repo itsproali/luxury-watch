19645881e2013b3cde59ec50c0a696ed9f2e9959
Mohammad Ali <itsproali@gmail.com>
2022-04-04 13:40:57 +0600

    Dashboard Page Added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -17,7 +17,7 @@ const Home = () => {
                 <div className="home-text">
                     <h1 className="text-5xl font-bold text-pink-600">Premium Digital Watch</h1>
                     <p className='text-1xl my-4 font-medium text-gray-500'>Beautiful, bright AMOLED display and the broadest range of all-day health monitoring features keeps track of your energy levels, respiration, menstrual cycle, stress, sleep, estimated heart rate and more</p>
-                    <button className="font-medium px-4 py-2 bg-pink-200 text-pink-500 cursor-pointer rounded hover:bg-pink-300 hover:text-pink-700 ease-in-out duration-300">Live Demo</button>
+                    <button className="font-medium px-4 py-2 bg-pink-200 text-pink-500 cursor-pointer rounded hover:bg-pink-300 hover:text-pink-700 ease-in-out duration-300">Explore Now</button>
                 </div>
             </div>
 
